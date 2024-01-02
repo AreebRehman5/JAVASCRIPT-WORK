1c8f6de87ed5a73c36092a55c2ed4eb5432d0ee6
abdulwaheed <engnrwaheed47@gmail.com>
2024-01-02 22:13:56 +0500

    first commit

---
+++ Js/script.js
@@ -0,0 +1,151 @@
+var z = "Hello World"
+var x = 100.25
+document.write(z)
+document.write(x)
+
+var a = 10;
+var b = 4;
+document.write(a + b);
+document.write("<br>");
+a--;
+document.write(a + b);
+var a = 10;
+var b = 4;
+
+a -= b;
+a *= b
+
+document.write(a);
+var x = 50;
+console.log(x+28);
+var a = 10;
+var b = 10;
+console.log(a==b);
+console.log(2!=2);
+var a =100;
+var b =100;
+if(a == b){
+    console.log("A is Greater");
+}
+else{
+    console.log("A is not Greater");
+}
+var age =20;
+        if (age>=18 && age <=21) {
+            console.log("Yes you are eligible")
+        }
+        var a =10;
+        var b=15;
+        if (a>=8 || b<=14) {
+            console.log("Yes you are eligible")
+        }
+        var per =130;
+if (per>=80 && per <=100) {
+    console.log("You Are in a Merit");
+} else if (
+    per>=60 && per < 80)
+    {
+        console.log("You Are in a second Position");
+}
+else if (
+    per>=45 && per < 60)
+    {
+        console.log("You Are in a third Position");
+}
+else if (
+    per>=33 && per < 45)
+    {
+        console.log("You Are in a fourth Position");
+}
+else if (
+     per < 33)
+    {
+        console.log("You Are Fail");
+}
+else{
+    console.log("Please Enter a Valid Percentage")
+}
+var a = 100;
+var b;
+
+b= "Value is "+ (a> 120 ? "True": "False");
+console.log(b);
+let day = 0;
+switch (day) {
+  case 0:
+    day = "Sunday Monday";
+  
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+
+console.log( "Today is " + day);
+var a =10;
+var b =20;
+if (a>b) {
+    alert("A is Greater")
+} else {
+    alert("B is Greater")
+}
+var a = confirm('Do you like our website ?');
+if (a) {
+  alert("Thanks")
+} else {
+  alert("sorry")
+}
+var a = prompt('What is your Name ?')
+alert(a);
+function hello(){
+    document.write("Hellos Everybody")
+}
+function areeb(){
+    document.write("Hello Areeb")
+}
+hello();
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+areeb();
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+hello();
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+hello();
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+hello();
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+
+document.write("Hello  ")
+document.write("Hello i am areeb")
+document.write("Hey!!")
+
+document.write("Hello World<br><br>");
+document.write("<i><b>Hello i am areeb</i><");
+
+document.write("Hey!!")
